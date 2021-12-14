8bdfb4efaa47de3f6be1634bca6f446ce1d8a847
Syed Ameenuddin <ameen.syed124@gmail.com>
2021-12-13 23:29:19 -0800

    Create twosum.py
    Shivam is the youngest programmer in the world, he is just 12 years old. Shivam is learning programming and today he is writing his first program.
    
    The task is very simple: given two integers A and B, write a program to add these two numbers and output it.
    
    Input
    This section tells you the format in which your program should receive the input.
    The first line contains an integer T, the total number of test cases. Then follow T lines, each line contains two Integers A and B.
    
    Output
    This section tells us the format in which your program should give the output
    For each test case, add A and B and display the sum in a new line.
    
    Take special care for the output format; everything your program prints is considered “output”, so if you output some debugging statements like “Please enter T” or print something like: “The answer is: ”, this will be considered as part of your answer, and because it does not satisfy the output format, it will be marked wrong, even if your answer is otherwise correct!
    Constraints
    This section tell you the maximum and minimum possible values the variables in the problem statement can take. You do not need to check these constraints in your program. You can safely assume that the input given to your program will be in the given range of constraints.
    1 ≤ T ≤ 1000
    0 ≤ A,B ≤ 10000
    Sample Input 1
    3
    1 2
    100 200
    10 40
    Sample Output 1
    3
    300
    50

---
+++ twosum.py
@@ -0,0 +1,6 @@
+t=int(input())
+#t take for loop time. if t=3. it will take three time the loop
+for i in range(t):
+    a,b=map(int, input().split())
+    #a,b take input value of a and b
+    print(a+b)
